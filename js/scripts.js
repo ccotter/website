6c9c116090569238b85ca9a7d2fca46c2db0b9bf
Chris Cotter <ccotter@utexas.edu>
2013-02-17 12:24:28 -0600

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+	
+	/**
+	 * Include this for Color Box
+	 */
+	// $('.colorbox').colorbox();	
+
+	/**
+	 * Include this to validate a form
+	 */
+	//$("#form").validate();
+	
+});
